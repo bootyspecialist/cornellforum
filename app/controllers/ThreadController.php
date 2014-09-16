d850d7760c6a339e84708aacb6664a004d9ca977
Will Najar <will.najar@outlook.com>
2014-09-16 14:55:22 -0700

    save thread and implement slug

--- app/controllers/ThreadController.php
+++ app/controllers/ThreadController.php
@@ -8,22 +8,32 @@ class ThreadController extends BaseController {
 		} else {
 			App::abort(404);
 		}
-		$thread = Thread::find()
-		return view;
 	}
 
 	public function newThread() {
+
 		$input = Input::only('title', 'body');
-		$user = Sentry::getUser();
+		$user = Sentry::getUser(); //logged in user
+
 		$validator = Validator::make(
 			$input,
 			array(
 				'title' => array('required', 'between:15,150'),
-				'body' => array('required', 'between:25, 2500')
+				'body' => array('required', 'between:25,2500')
 			)
 		);
-
 		if ($validator->passes()) {
+			$new_thread = Thread::create(array(
+				'title' => e($input['title']),
+				//slugs auto-generated with eloquent-sluggable
+				'body_raw' => e($input['body']),
+				'body' => e($input['body']), //apply BBCode to generate HTML and store it
+				'user_id'=> $user->id
+				//points defaults to 1 via schema
+				//timestamps are automatically set to now()
+
+			));
+			return Redirect::to('thread/' . $new_thread->id . '/' . $new_thread->slug);
 		} else {
 			return Redirect::to('thread/new')->withInput()->withErrors($validator);
 		}
--- app/models/Thread.php
+++ app/models/Thread.php
@@ -1,7 +1,17 @@
 <?php
 
-class Thread extends Eloquent {
+use Cviebrock\EloquentSluggable\SluggableInterface;
+use Cviebrock\EloquentSluggable\SluggableTrait;
+
+class Thread extends Eloquent implements SluggableInterface {
+
+	use SluggableTrait;
 
 	protected $fillable = array('title', 'body_raw', 'body', 'user_id');
+	protected $sluggable = array(
+        'build_from' => 'title',
+        'save_to'    => 'slug',
+    );
+
 
 }
