05571778933b2f27987dd6ef407c10f95e949529
Will Najar <will.najar@outlook.com>
2014-09-16 16:25:45 -0700

    basic thread preview structure

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -3,6 +3,19 @@
 	Home
 @stop
 @section('content')
+	<div class="col-md-12">
+		@foreach ($threads as $thread)
+			<div class="thread-preview clearfix" id="thread-{{ $thread->id }}">
+				<div class="vote-arrow">
+					<i class="fa fa-angle-right"></i>
+				</div>
+				<div class="thread-index-content">
+					<h2 class="thread-index-title"><a href="/thread/{{ $thread->id }}/{{ $thread->slug }}">{{ $thread->title }}</a></h3>
+					<p class="thread-index-subtitle">{{ $thread->points }} points, x comments, posted y ago</p>
+				</div>
+			</div>
+		@endforeach
+	</div>
 	<div class="col-xs-12">
 		<div id="pagination">
 
