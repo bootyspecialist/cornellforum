84eca8a612764de700a4a878c01ca6aa0f30189d
Will Najar <will.najar@outlook.com>
2014-09-16 02:41:58 -0700

    basic new thread logic

--- app/controllers/ThreadController.php
+++ app/controllers/ThreadController.php
@@ -7,7 +7,22 @@ class ThreadController extends BaseController {
 	}
 
 	public function newThread() {
-		return 0;
+		$input = Input::only('title', 'body');
+		$user = Sentry::getUser();
+		$validator = Validator::make(
+			$input,
+			array(
+				'title' => array('required', 'between:15,150'),
+				'body' => array('required', 'between:25, 2500')
+			)
+		);
+
+		if ($validator->passes()) {
+    		//success! create new thread
+		} else {
+			//failure, return with errors
+			return Redirect::to('thread/new')->withInput()->withErrors($validator);
+		}
 	}
 
 }
