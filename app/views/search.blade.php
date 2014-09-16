700802ea09798652bbcc4b14149b265a4d20f16c
Will Najar <will.najar@outlook.com>
2014-09-16 03:58:31 -0700

    change search layout to match rest of pages

--- app/views/search.blade.php
+++ app/views/search.blade.php
@@ -3,13 +3,16 @@
 	Search
 @stop
 @section('content')
-	<form role="form" action="/search" method="get">
-		<div class="form-group">
-			<label for="search-term" class="sr-only">Search term:</label>
-    		<input type="text" class="form-control input-lg" id="search-term" placeholder="Search for something interesting...">
+	<div class="col-md-12">
+		<h4 class="spacer">Search for something interesting:</h4>
+		<form role="form" action="/search" method="get">
+			<div class="form-group">
+				<label for="search-term" class="sr-only">Search term:</label>
+	    		<input type="text" class="form-control input-lg" id="search-term" placeholder="jeff seid flexing shirtless tight briefs no homo">
+			</div>
+		</form>
+		<div id="search-results">
+			<p class="text-muted">Results will populate here as you search...</p>
 		</div>
-	</form>
-	<div id="search-results">
-		<p class="text-muted">Results will populate here as you search...</p>
 	</div>
 @stop
