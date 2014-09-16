691fca5c7f0b200150b620b9b1a11e5ff2fbd4e7
Will Najar <will.najar@outlook.com>
2014-09-16 16:33:31 -0700

    new comment form and styling

--- app/views/thread.blade.php
+++ app/views/thread.blade.php
@@ -18,8 +18,42 @@
 			<a class="posting-tips pull-right" target="_blank" href="/frequently-asked-questions">want some posting tips?</a>
 			@if(Sentry::check())
 				<div id="new-comment-form">
-					<form action="/comment/{{ $thread->id }}/new/" method="post" role="form">
-					</form>
+					{{ Form::open(array('action' => 'CommentController@newComment')) }}
+						<div class="outer-textarea-container">
+							<div class="form-group">
+								<label class="sr-only" for="body">Thread body:</label>
+								{{ Form::textarea('body', null, array('class' => 'form-control with-formatting-buttons', 'rows' => 12, 'placeholder' => 'Compose something interesting here...', 'autofocus')) }}
+							</div>
+							<div class="formatting-buttons">
+								<ul>
+									<li title="insert bolded text" class="formatting-button bold" data-action="bold">
+										<i class="fa fa-bold"></i>
+									</li>
+									<li title="insert italicized text" class="formatting-button italic" data-action="italic">
+										<i class="fa fa-italic"></i>
+									</li>
+									<li title="insert an image" class="formatting-button image" data-action="image">
+										<i class="fa fa-picture-o"></i>
+									</li>
+									<li title="insert a youtube video" class="formatting-button youtube" data-action="youtube">
+										<i class="fa fa-youtube"></i>
+									</li>
+									<li title="insert a quote" class="formatting-button quote" data-action="quote">
+										<i class="fa fa-quote-left"></i>
+									</li>
+									<li title="help" class="formatting-help">
+										<a target="_blank" href="/frequently-asked-questions"><i class="fa fa-question-circle"></i></a>
+									</li>
+								</ul>
+							</div>
+						</div>
+						<div class="form-group errors">
+							{{ ($errors->has('body') ?  $errors->first('body') : '') }}
+						</div>
+						<div class="form-group">
+							{{ Form::submit('Post comment', array('class' => 'btn btn-primary'))}}
+						</div>
+					{{ Form::close() }}
 				</div>
 			@else
 				<div id="new-comment-form">
