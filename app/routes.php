e8a2337be2a61d839701b14c87d06d36897073f9
Will Najar <will.najar@outlook.com>
2014-09-15 19:16:45 -0700

    Revert "Revert "confide...""
    This reverts commit d47971b648571c285de9c889b97d62c6fc105848.

--- app/routes.php
+++ app/routes.php
@@ -42,9 +42,14 @@ Route::get('frequently-asked-questions', function() {
 Route::get('search', 'SearchController@searchPage');
 Route::get('search/{query}', 'SearchController@search');
 
-//Login, create account, logout
-Route::get('account/login', 'UserController@loginPage');
-Route::post('account/login', 'UserController@login');
-Route::get('account/signup', 'UserController@signupPage');
-Route::post('account/login', 'UserController@login');
-Route::get('logout', 'UserController@logout');
+// Confide routes (user management)
+Route::get('users/create', 'UsersController@create');
+Route::post('users', 'UsersController@store');
+Route::get('users/login', 'UsersController@login');
+Route::post('users/login', 'UsersController@doLogin');
+Route::get('users/confirm/{code}', 'UsersController@confirm');
+Route::get('users/forgot_password', 'UsersController@forgotPassword');
+Route::post('users/forgot_password', 'UsersController@doForgotPassword');
+Route::get('users/reset_password/{token}', 'UsersController@resetPassword');
+Route::post('users/reset_password', 'UsersController@doResetPassword');
+Route::get('users/logout', 'UsersController@logout');
