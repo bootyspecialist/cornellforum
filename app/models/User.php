e9c542c71bd4d1ec75871ac3c49c72743f35f9fd
Will Najar <will.najar@outlook.com>
2014-09-17 11:42:49 -0700

    add user model to possibly extend

--- app/models/User.php
+++ app/models/User.php
@@ -1,29 +1,9 @@
-<?php
+<?php namespace App\Models;
 
-use Illuminate\Auth\UserTrait;
+use Eloquent;
 use Illuminate\Auth\UserInterface;
-use Illuminate\Auth\Reminders\RemindableTrait;
 use Illuminate\Auth\Reminders\RemindableInterface;
 
-class User extends Eloquent implements UserInterface, RemindableInterface {
-
-	use UserTrait, RemindableTrait;
-
-	/**
-	 * The database table used by the model.
-	 *
-	 * @var string
-	 */
-	protected $table = 'users';
-
-	/**
-	 * The attributes excluded from the model's JSON form.
-	 *
-	 * @var array
-	 */
-	protected $hidden = array('password', 'remember_token');
-
-	//attributes we can fill from a form
-	protected $fillable = array('email', 'password');
+class User extends \Cartalyst\Sentry\Users\Eloquent\User implements UserInterface, RemindableInterface {
 
 }
