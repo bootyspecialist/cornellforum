31275be21e828592ba5a4885a68f6b890285ca6d
Will Najar <will.najar@outlook.com>
2014-09-17 16:09:09 -0700

    touch Thread model when a comment in the thread posted

--- app/models/Comment.php
+++ app/models/Comment.php
@@ -3,9 +3,10 @@
 class Comment extends Eloquent {
 
 	protected $fillable = array('body_raw', 'body', 'user_id', 'thread_id');
+	protected $touches = array('Thread');
 
 	public function thread() {
-        return $this->hasOne('Thread');
+        return $this->belongsTo('Thread');
     }
 
 }
