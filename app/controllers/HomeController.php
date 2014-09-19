2e969ac5e7fa246eb01d6c87f30bb2c0cc3c9e7b
Will Najar <will.najar@outlook.com>
2014-09-18 18:47:42 -0700

    delete boilerplate

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -2,19 +2,6 @@
 
 class HomeController extends BaseController {
 
-	/*
-	|--------------------------------------------------------------------------
-	| Default Home Controller
-	|--------------------------------------------------------------------------
-	|
-	| You may wish to use controllers instead of, or in addition to, Closure
-	| based routes. That's great! Here is an example controller method to
-	| get you started. To route to this controller, just add the route:
-	|
-	|	Route::get('/', 'HomeController@showWelcome');
-	|
-	*/
-
 	public function homePage() {
 		$threads = Thread::orderBy('updated_at', 'desc')->take(25)->get();
 		return View::make('home', array('threads' => $threads));
