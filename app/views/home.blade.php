dbb13433911f466b5b2c87ee9bb91fb42d9d09af
Will Najar <will.najar@outlook.com>
2014-09-20 20:31:34 -0700

    fix style of indicators

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -10,9 +10,9 @@
 					<h2 class="thread-index-title">
 						<a href="/thread/{{{ $thread->id }}}/{{{ $thread->slug }}}">{{{ $thread->title }}}</a>
 						@if(Sentry::check() && !Threadview::where('user_id', '=', Sentry::getUser()->id)->where('thread_id', '=', $thread->id)->exists() && $thread->updated_at->diff(\Carbon\Carbon::now())->days <= 7)
-							<span class="new-thread-notification bg-info text-primary">new!</span>
+							<span class="new-thread-notification text-primary">new!</span>
 						@elseif(Sentry::check() && Threadview::where('user_id', '=', Sentry::getUser()->id)->where('thread_id', '=', $thread->id)->where('updated_at', '<', $thread->updated_at)->exists() && $thread->updated_at->diff(\Carbon\Carbon::now())->days <= 7)
-							<span class="new-comment-notification text-muted">updated</span>
+							<span class="new-comment-notification text-muted">new comments!</span>
 						@endif
 					</h3>
 					<p class="thread-index-subtitle">{{{ $thread->points }}} {{{ Lang::choice('point|points', $thread->points) }}}, {{{ $thread->comments()->count() }}} {{{ Lang::choice('comment|comments', $thread->comments()->count()) }}}, posted {{{ $thread->created_at->diffForHumans() }}}</p>
