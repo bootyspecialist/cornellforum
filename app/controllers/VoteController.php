6362c4b70fcc694761b04d7f7279338e8b5714fe
Will Najar <will.najar@outlook.com>
2014-09-20 17:08:54 -0700

    improve up/down vote logic

--- app/controllers/VoteController.php
+++ app/controllers/VoteController.php
@@ -9,9 +9,20 @@ class VoteController extends BaseController {
 
 		$user = Sentry::getUser();
 		if (Vote::where('user_id', '=', $user->id)->where('thread_id', '=', $thread->id)->exists()) {
-			return "You have already voted on this thread!";
+			if ($old_vote = Vote::where('user_id', '=', $user->id)->where('thread_id', '=', $thread->id)->first()->sign == 1) {
+				//already upvoted
+				return "You have already voted this thread up!";
+			} else {
+				//prevously downvoted, let's change it to an upvote
+				$old_vote->sign = 1;
+				$old_vote->save();
+				$thread->points = $thread->points + 2; //subtract two from thread points (erase previous downvote)
+				$thread->save();
+				return Redirect::to('thread/' . $thread->id . '/' . $thread->slug); //don't use Redirect::back()
+			}
 		}
 
+		//no existing vote, create one
 		$new_vote = Vote::create(array(
 			'user_id' => $user->id,
 			'thread_id' => $thread->id,
@@ -27,6 +38,10 @@ class VoteController extends BaseController {
 	}
 
 	public function voteDown($thread_id) {
+		if(!$thread = Thread::find($thread_id)) { //make sure thread exists
+			return "You can't vote on a thread that doesn't exist.";
+		}
+
 		$user = Sentry::getUser();
 		//can only do this if account at least 30 days old
 		if (!$user->created_at->diff(\Carbon\Carbon::now())->days >= 30) {
@@ -34,7 +49,17 @@ class VoteController extends BaseController {
 		}
 
 		if (Vote::where('user_id', '=', $user->id)->where('thread_id', '=', $thread->id)->exists()) {
-			return "You have already voted on this thread!";
+			if ($old_vote = Vote::where('user_id', '=', $user->id)->where('thread_id', '=', $thread->id)->first()->sign == -1) {
+				//already downvoted
+				return "You have already voted this thread down!";
+			} else {
+				//prevously downvoted, let's change it to an upvote
+				$old_vote->sign = -1;
+				$old_vote->save();
+				$thread->points = $thread->points - 2; //subtract two from thread points (erase previous upvote)
+				$thread->save();
+				return Redirect::to('thread/' . $thread->id . '/' . $thread->slug); //don't use Redirect::back()
+			}
 		}
 
 		$new_vote = Vote::create(array(
