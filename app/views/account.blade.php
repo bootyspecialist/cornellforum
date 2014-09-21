7e2d7147f3f4e1d6d03ee750407c8e8607b270a2
Will Najar <will.najar@outlook.com>
2014-09-21 02:07:30 -0700

    escape email

--- app/views/account.blade.php
+++ app/views/account.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 	<div class="col-md-12">
 		<h4 class="spacer">Account information:</h4>
-		<p><i class="fa fa-check-square"></i> <strong>{{ Sentry::getUser()->email }}</strong></p>
+		<p><i class="fa fa-check-square"></i> <strong>{{{ Sentry::getUser()->email }}}</strong></p>
 		<ul class="list-unstyled">
 			<li>Your email is verified and you can post/comment as much as you like.</li>
 			<li>This information will <u>never</u> be shown next to threads or posts you make.</li>
