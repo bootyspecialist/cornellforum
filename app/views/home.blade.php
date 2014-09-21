b3e9f914650eaf3c710d8f11ddd4346ab22536b1
Will Najar <will.najar@outlook.com>
2014-09-20 20:07:49 -0700

    new thread and new comments indicators on homepage

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -7,7 +7,14 @@
 		@foreach ($threads as $thread)
 			<div class="thread-preview clearfix" id="thread-{{{ $thread->id }}}">
 				<div class="thread-index-content">
-					<h2 class="thread-index-title"><a href="/thread/{{{ $thread->id }}}/{{{ $thread->slug }}}">{{{ $thread->title }}}</a></h3>
+					<h2 class="thread-index-title">
+						<a href="/thread/{{{ $thread->id }}}/{{{ $thread->slug }}}">{{{ $thread->title }}}</a>
+						@if(Sentry::check() && !Threadview::where('user_id', '=', Sentry::getUser()->id)->where('thread_id', '=', $thread->id)->exists() && $thread->updated_at->diff(\Carbon\Carbon::now())->days <= 7)
+							<span class="new-thread-notification bg-info text-primary">new!</span>
+						@elseif(Sentry::check() && Threadview::where('user_id', '=', Sentry::getUser()->id)->where('thread_id', '=', $thread->id)->where('updated_at', '<', $thread->updated_at)->exists() && $thread->updated_at->diff(\Carbon\Carbon::now())->days <= 7)
+							<span class="new-comment-notification text-muted">updated</span>
+						@endif
+					</h3>
 					<p class="thread-index-subtitle">{{{ $thread->points }}} {{{ Lang::choice('point|points', $thread->points) }}}, {{{ $thread->comments()->count() }}} {{{ Lang::choice('comment|comments', $thread->comments()->count()) }}}, posted {{{ $thread->created_at->diffForHumans() }}}</p>
 				</div>
 			</div>
