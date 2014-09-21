23d334d5232177fac47ccc38e19977700015aa23
Will Najar <will.najar@outlook.com>
2014-09-20 22:53:05 -0700

    25 threads/page

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -3,7 +3,7 @@
 class HomeController extends BaseController {
 
 	public function homePage() {
-		$threads = Thread::orderBy('updated_at', 'desc')->paginate(5);
+		$threads = Thread::orderBy('updated_at', 'desc')->paginate(25);
 		return View::make('home', array('threads' => $threads));
 	}
 
