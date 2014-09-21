7c95161003fc54d129be19b50b5f0a5c3b8e89c0
Will Najar <will.najar@outlook.com>
2014-09-21 02:36:15 -0700

    fixing up routes to control auth before certain actions

--- app/routes.php
+++ app/routes.php
@@ -11,42 +11,66 @@
 |
 */
 
-//Homepage and subsequent pages
+//homepage and subsequent pages
 Route::get('/', array(
 	'as' => 'home',
 	'uses' => 'HomeController@homePage'
 ));
 
 //threads
-Route::get('thread/new', function() { return View::make('newthread'); });
-Route::post('thread/new', 'ThreadController@newThread');
+Route::get('thread/new', array(
+	'as' => 'new_thread_form',
+	'before' => 'Sentinel\auth',
+	'uses' => 'ThreadController@newThreadForm'
+));
+Route::post('thread/new', array(
+	'as' => 'post_new_thread',
+	'before' => 'Sentinel\auth',
+	'uses' => 'ThreadController@newThread',
+));
 Route::get('thread/{thread_id}/{slug?}', array(
 	'as' => 'thread',
 	'uses' => 'ThreadController@viewThread'
 ))->where('thread_id', '[0-9]+');
 Route::get('delete/thread/{thread_id}', array(
 	'as' => 'delete_thread',
+	'before' => 'Sentinel\auth',
 	'uses' => 'ThreadController@deleteThread'
 ))->where('thread_id', '[0-9]+');
 Route::get('quote/thread/{thread_id}', array(
 	'as' => 'quote_thread',
+	'before' => 'Sentinel\auth',
 	'uses' => 'ThreadController@quoteThread'
 ))->where('thread_id', '[0-9]+');
 
 //comments
-Route::post('comment/{thread_id}/new', 'CommentController@newComment');
+Route::post('comment/{thread_id}/new', array(
+	'as' => 'post_new_comment',
+	'before' => 'Sentinel\auth',
+	'uses' => 'CommentController@newComment'
+))->where('thread_id', '[0-9]+');
 Route::get('delete/comment/{comment_id}', array(
 	'as' => 'delete_comment',
+	'before' => 'Sentinel\auth',
 	'uses' => 'CommentController@deleteComment'
 ))->where('comment_id', '[0-9]+');
 Route::get('quote/comment/{comment_id}', array(
 	'as' => 'quote_comment',
+	'before' => 'Sentinel\auth',
 	'uses' => 'CommentController@quoteComment'
 ))->where('comment_id', '[0-9]+');
 
 //voting
-Route::get('vote/{thread_id}/up', 'VoteController@voteUp');
-Route::get('vote/{thread_id}/down', 'VoteController@voteDown');
+Route::get('vote/{thread_id}/up', array(
+	'as' => 'vote_up',
+	'before' => 'Sentinel\auth',
+	'uses' => 'VoteController@voteUp'
+))->where('thread_id', '[0-9]+');
+Route::get('vote/{thread_id}/down', array(
+	'as' => 'vote_down',
+	'before' => 'Sentinel\auth',
+	'uses' => 'VoteController@voteDown'
+))->where('thread_id', '[0-9]+');
 
 //vanity pages
 Route::get('frequently-asked-questions', function() {
@@ -60,9 +84,11 @@ Route::get('search', function() {
 Route::get('search/{query}', 'SearchController@search');
 
 //profile page
-Route::get('account', function() {
-	return View::make('account');
-});
+Route::get('account', array(
+	'as' => 'account_view',
+	'before' => 'Sentinel\auth',
+	'uses' => function() {return View::make('account');}
+));
 Route::get('verification-sent', array('as' => 'verification-sent', function() {
 	return View::make('verificationsent');
 }));
