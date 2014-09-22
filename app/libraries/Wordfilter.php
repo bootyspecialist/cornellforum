ee5bb1e444c2edb92e57dd86c432f1d86038e1c7
Will Najar <will.najar@outlook.com>
2014-09-21 18:55:34 -0700

    expand and fix wordfilter

--- app/libraries/Wordfilter.php
+++ app/libraries/Wordfilter.php
@@ -9,16 +9,19 @@ class Wordfilter {
             '/s+h+i+t+/i', /* shit case-insensitive and multiple letter repeats */
             '/(f+|ph)[a4]+g+[o0]+t+/i', /* faggot or phaggot or f4ggot of ph4ggot case-insensitive and multiple letter repeats*/
             '/(f+|ph)[a4]+g+/i', /* fag or phag or f4g or ph4g case-insensitive and multiple letter repeats */
-            '/s+l+([o0]{2,}|u+)t+/i' /* sloot or slut or sl00t case-insensitive and multiple letter repeats */
+            '/s+l+([o0]{2,}|u+)t+/i', /* sloot or slut or sl00t case-insensitive and multiple letter repeats */
+            '/n+i+g+e+r+/i' /* the n word */
+
         );
 
         $replacements = array (
-            'poppycock', /* tribute to Poseidon */
-            'poppycock', /* see above */
-            'chitty chitty bang bang', /* Mary Poppins... always a classic */
-            'fratstar', /* Ezra Hub legend */
-            'fratstar', /* see above */
-            'slot machine' /* put your coin in, pull the lever, take a chance... see what happens? */
+            '****',
+            '****',
+            '****',
+            '******',
+            '******',
+            '****',
+            '******'
         );
 
         $text = preg_replace($patterns_to_filter, $replacements, $text);
