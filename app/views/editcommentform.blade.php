0133efe32d67b513951991b3507e3060848cb680
Will Najar <will.najar@outlook.com>
2014-10-21 02:31:33 -0700

    fixing edit comment form

--- app/views/editcommentform.blade.php
+++ app/views/editcommentform.blade.php
@@ -3,34 +3,12 @@
 		<div class="outer-textarea-container">
 			<div class="form-group">
 				<label class="sr-only" for="body">Comment body:</label>
-				<textarea class="form-control with-formatting-buttons" rows="12" name="body" cols="50">{{ $comment->body_raw }}</textarea>
-			</div>
-			<div class="formatting-buttons">
-				<ul>
-					<li title="insert bolded text" class="formatting-button bold" data-action="bold">
-						<i class="fa fa-bold"></i>
-					</li>
-					<li title="insert italicized text" class="formatting-button italic" data-action="italic">
-						<i class="fa fa-italic"></i>
-					</li>
-					<li title="insert an image" class="formatting-button image" data-action="image">
-						<i class="fa fa-picture-o"></i>
-					</li>
-					<li title="insert a youtube video" class="formatting-button youtube" data-action="youtube">
-						<i class="fa fa-youtube"></i>
-					</li>
-					<li title="insert a quote" class="formatting-button quote" data-action="quote">
-						<i class="fa fa-quote-left"></i>
-					</li>
-					<li title="help" class="formatting-help">
-						<a target="_blank" href="/frequently-asked-questions"><i class="fa fa-question-circle"></i></a>
-					</li>
-				</ul>
+				<textarea class="form-control" rows="6" name="body" cols="50">{{ $comment->body_raw }}</textarea>
 			</div>
 		</div>
 		<div class="form-group">
-			{{ Form::submit('Post edited comment', array('class' => 'btn btn-primary')) }}
-			<span class="btn btn-default">Don't save and go back</span>
+			{{ Form::submit('Post edited comment', array('class' => 'btn btn-warning btn-sm')) }}
+			<span class="btn btn-default btn-sm">Don't save and go back</span>
 		</div>
 	{{ Form::close() }}
 </div>
