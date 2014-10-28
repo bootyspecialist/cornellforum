7e7bfcbbcc5c6d00af165138b3bd779fada4e154
Will Najar <will.najar@outlook.com>
2014-10-27 17:33:35 -0700

    streamline comment form when not logged in

--- app/views/thread.blade.php
+++ app/views/thread.blade.php
@@ -119,33 +119,11 @@
 					<div class="outer-textarea-container">
 						<div class="form-group">
 							<label class="sr-only" for="body">Thread body:</label>
-							{{ Form::textarea('body', null, array('class' => 'form-control with-formatting-buttons', 'rows' => 12, 'placeholder' => 'Whoops! You need to log in or create an account to comment on this thread...', 'disabled')) }}
-						</div>
-						<div class="formatting-buttons">
-							<ul>
-								<li title="insert bolded text" class="formatting-button bold" data-action="bold">
-									<i class="fa fa-bold"></i>
-								</li>
-								<li title="insert italicized text" class="formatting-button italic" data-action="italic">
-									<i class="fa fa-italic"></i>
-								</li>
-								<li title="insert an image" class="formatting-button image" data-action="image">
-									<i class="fa fa-picture-o"></i>
-								</li>
-								<li title="insert a youtube video" class="formatting-button youtube" data-action="youtube">
-									<i class="fa fa-youtube"></i>
-								</li>
-								<li title="insert a quote" class="formatting-button quote" data-action="quote">
-									<i class="fa fa-quote-left"></i>
-								</li>
-								<li title="help" class="formatting-help">
-									<a target="_blank" href="/frequently-asked-questions"><i class="fa fa-question-circle"></i></a>
-								</li>
-							</ul>
+							{{ Form::textarea('body', null, array('class' => 'form-control', 'rows' => 6, 'placeholder' => 'Whoops! You need to log in or create an account to comment on this thread. Creating an account is quick and easy, we promise! See the buttons below to get started.', 'disabled')) }}
 						</div>
 					</div>
 					<div class="form-group">
-						<p class="need-to-login"><a class="btn btn-primary" href="/login"><i class="fa fa-sign-in"></i> Login</a> <a class="btn btn-primary" href="/create-account"><i class="fa fa-user"></i> Create an account</a> Creating an account is quick and easy, we promise!</p>
+						<p class="need-to-login"><a class="btn btn-primary" href="/login"><i class="fa fa-sign-in"></i> Login</a> <a class="no-account-yet" href="/create-account">Don't have an account yet?</a></p>
 					</div>
 				</div>
 			@endif
